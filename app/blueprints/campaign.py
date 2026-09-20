f62e1b45327803142d1b4c13df2680496abb61c5
r4rct0 <r4rcto@proton.mail>
2026-09-20 12:22:19 -0400

    changed the big button on charity-info to be a direct donate link instead of the same basic charity url

--- app/blueprints/campaign.py
+++ app/blueprints/campaign.py
@@ -53,6 +53,6 @@ def get_campaign_info():
             "startDate": settings.get("Auction Start Time", ""),
             "endDate": settings.get("Auction End Time", ""),
             "charityLogoUrl": settings.get("Charity Logo", ""),
-            "charityWebsite": settings.get("Charity Website", ""),
+            "charityWebsite": settings.get("Charity Direct Donate Link", ""),
         }
     ), 200

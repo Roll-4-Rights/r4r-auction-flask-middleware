81ece69de85ef8c7cb81dc47c69381141d250da4
r4rct0 <r4rcto@proton.mail>
2026-09-24 08:01:05 -0400

    adding eventlet

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:5000 app:app
+web: gunicorn --worker-class eventlet -w 1 --bind 0.0.0.0:5000 app:app
--- app/extensions.py
+++ app/extensions.py
@@ -12,7 +12,7 @@ socketio = SocketIO()
 def init_extensions(app):
     login_manager.init_app(app)
     limiter.init_app(app)
-    socketio.init_app(app, cors_allowed_origins=app.config["ALLOWED_ORIGINS"], async_mode="threading")
+    socketio.init_app(app, cors_allowed_origins=app.config["ALLOWED_ORIGINS"], async_mode="eventlet")
 
     @login_manager.unauthorized_handler
     def unauthorized():
